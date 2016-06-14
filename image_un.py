f2931c5bcc1d21acf870c72196f5d2540f6187a7
bxk218 <bxk218@gmail.com>
2016-06-14 16:54:57 +0900

    Some modification on image. XML example is added.

--- image_un.py
+++ image_un.py
@@ -14,3 +14,7 @@ example_file = ("http://upload.wikimedia.org/wikipedia/commons/7/7d/Dog_face.png
 image = imread(example_file, as_grey=True)
 plt.imshow(image, cmap=cm.gray)
 plt.show()
+
+image2 = image[5:70, 0:70]
+plt.imshow(image2, cmap=cm.gray)
+plt.show()
---
+++ web_xml.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 10:17:54 2016
+
+@author: Administrator
+"""
+
+from lxml import objectify
+import pandas as pd
+
+xml = objectify.parse(open('XMLData.xml'))
+root = xml.getroot()
+
+df = pd.DataFrame(columns=('Number','String','Boolean'))
+
+for i in range(0,4):
+    Obj = root.getchildren()[i].getchildren()
+    row = dict(zip(['Number', 'String', 'Boolean'],
+                   [Obj[0].text, Obj[1].text, Obj[2].text]))
+    row_s = pd.Series(row)
+    row_s.name = i
+    df = df.append(row_s)
+    
+print(df)
